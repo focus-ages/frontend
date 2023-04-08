159b4c3559fbb6547f0fa139ed8c669ba876d4c3
Guilherme Argilar <guilherme.argilar@gmail.com>
2023-04-08 06:42:31 -0300

    criando o botões de editar notificacao/alterar Meta diária e alterando o countdowntimer para que apareça o tempo atual e  meta diaria

--- lib/components/countdown_timer.dart
+++ lib/components/countdown_timer.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 
 import '../resources/color_pattern.dart';
 
@@ -14,31 +16,55 @@ class CountdownTimer extends StatefulWidget {
 
 class _CountdownTimerState extends State<CountdownTimer> {
   final int _duration = 10;
+  int currentDuration = 0;
   @override
   Widget build(BuildContext context) {
-    return CircularCountDownTimer(
-      //width: MediaQuery.of(context).size.width / 2,
-      //height: MediaQuery.of(context).size.height / 2,
-      width: 180,
-      height: 180,
-      duration: _duration,
-      fillColor: ColorPattern.green,
-      ringColor: ColorPattern.darkCard,
-      strokeCap: StrokeCap.round,
-      strokeWidth: 20.0,
-      isTimerTextShown: true,
-      fillGradient: LinearGradient(colors: const [
-        Color(0xff23B854),
-        Color(0xff346B47),
-        Color(0xff2DEB6C),
-        Color(0xff75EE9D),
-        Color(0xff27CC5D),
-      ]),
+    return Stack(alignment: Alignment.center, children: [
+      CircularCountDownTimer(
+        //width: MediaQuery.of(context).size.width / 2,
+        //height: MediaQuery.of(context).size.height / 2,
+        width: 180,
+        height: 180,
+        duration: _duration,
+        fillColor: ColorPattern.green,
+        ringColor: ColorPattern.darkCard,
+        strokeCap: StrokeCap.round,
+        strokeWidth: 20.0,
+        isTimerTextShown: true,
+        fillGradient: LinearGradient(colors: const [
+          Color(0xff23B854),
+          Color(0xff346B47),
+          Color(0xff2DEB6C),
+          Color(0xff75EE9D),
+          Color(0xff27CC5D),
+        ]),
+        //faz isso funcionar
+        onChange: (String timeStamp) {
 
-      textStyle: const TextStyle(
-        fontSize: 36,
-        color: Colors.white,
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            setState(() {
+              currentDuration = int.parse(timeStamp);
+            });
+          });
+
+        },
       ),
-    );
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(currentDuration.toString() + " Min",
+              style: const TextStyle(
+                fontSize: 36,
+                color: ColorPattern.white,
+              )),
+          Text(_duration.toString() + " Min",
+          style: const TextStyle(
+            fontSize: 24,
+            color: ColorPattern.gray,
+          )),
+        ],
+      ),
+      
+    ]);
   }
 }
--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 import 'package:front/components/card_frases/card_frases.dart';
 import 'package:front/components/countdown_timer.dart';
 import 'package:front/components/switch_button.dart';
 
 import '../../components/card_frases/floating.dart';
+import '../../components/notification_button.dart';
 import '../../resources/color_pattern.dart';
 
 class HomePage extends StatefulWidget {
@@ -32,6 +35,7 @@ class _HomePageState extends State<HomePage> {
           Center(
               child: Column(
             children: [
+              
               const CountdownTimer(),
               const Padding(padding: EdgeInsets.only(bottom: 20)),
               const Text('Tempo Usado',
@@ -42,18 +46,20 @@ class _HomePageState extends State<HomePage> {
                   )),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                      Padding(padding: EdgeInsets.only(left: 5)),
+                children:  [
+                      const Padding(padding: EdgeInsets.only(left: 5)),
 
-                  Text('Meta Diária',
-                      style: TextStyle(
-                        color: ColorPattern.gray,
-                        fontSize: 18,
-                      )),
-                  Icon(Icons.edit, color: ColorPattern.gray, size: 18),
+                  TextButton(
+                    onPressed: (){},
+                    child: const Text('Meta Diária',
+                        style: TextStyle(
+                          color: ColorPattern.gray,
+                          fontSize: 18,
+                        )),
+                  ),
+                  const Icon(Icons.edit, color: ColorPattern.gray, size: 18),
                 ],
               ),
-              const Padding(padding: EdgeInsets.only(bottom: 20)),
             ],
           )),
           Padding(
@@ -91,8 +97,8 @@ class _HomePageState extends State<HomePage> {
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.start,
-                    children: const [
-                      Text(
+                    children:  [
+                       const Text(
                         "Tempo de \nNotificação",
                         style: TextStyle(
                           fontSize: 15,
@@ -100,17 +106,10 @@ class _HomePageState extends State<HomePage> {
                           color: Colors.white,
                         ),
                       ),
-                      Padding(padding: EdgeInsets.only(left: 60)),
-                      Text(
-                        "10 Min",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      Padding(padding: EdgeInsets.only(left: 3)),
-                      Icon(Icons.edit, color: ColorPattern.white, size: 18),
+                      const Padding(padding: EdgeInsets.only(left: 60)),
+                      // ignore: prefer_const_constructors
+                      NotificationButton(),
+                      
                     ],
                   ),
                   const Padding(padding: EdgeInsets.only(bottom: 10)),
