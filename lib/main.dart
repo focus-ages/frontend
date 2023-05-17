6fb742ac656a9a50a0ca3e92f5238dba1a7f228a
Luis Felipe Copetti Rossi <luisrossi@Luiss-MacBook-Pro.local>
2023-05-17 18:02:35 -0300

    local notification

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:front/firebase_options.dart';
+import 'package:front/resources/notification/local_notification.dart';
 import 'package:front/view/app_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
 
@@ -10,32 +11,9 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  
-  AndroidInitializationSettings androidSettings =
-      const AndroidInitializationSettings('@mipmap/ic_launcher');
-
-  DarwinInitializationSettings iosSettings =
-    const DarwinInitializationSettings(
-      requestAlertPermission: true,
-      requestBadgePermission: true,
-      requestCriticalPermission: true,
-      requestSoundPermission: true
-    );
-
-  InitializationSettings initializationSettings =
-    InitializationSettings(
-      android: androidSettings,
-      iOS: iosSettings
-    );
-
-  bool? initialized =  await notificationsPlugin.initialize(
-      initializationSettings
-  );
-
-  log("Notification: $initialized");
-
   await Firebase.initializeApp(options: 
   DefaultFirebaseOptions.currentPlatform);
+  notification();
   runApp(const AppWidget());
 
 }
