e82b19c9b23290fb75200053128c98bd53fe5008
Luis Felipe Copetti Rossi <luisrossi@Luiss-MacBook-Pro.local>
2023-05-17 18:56:46 -0300

    rotacao

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:front/firebase_options.dart';
 import 'package:front/view/app_widget.dart';
 import 'package:firebase_core/firebase_core.dart';
-
+import 'package:flutter/services.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
   runApp(const AppWidget());
 }
