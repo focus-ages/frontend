0525378e496085024ddbf0573ea203ce5ccb6603
Pedro Iglesias <pedro.iglesias@edu.pucrs.br>
2023-05-18 15:54:07 -0300

    accessibility service tests

--- android/app/src/main/kotlin/com/example/front/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/front/MainActivity.kt
@@ -1,29 +1,29 @@
 package com.example.front
 
-import android.os.Build
-import android.view.WindowManager
-import android.graphics.ColorMatrix
-import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
 class MainActivity : FlutterActivity() {
-  private val CHANNEL = "channel_name"
+    private val CHANNEL = "channel_name"
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-      super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-      MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-          if (call.method == "enableGrayscale") {
-              GrayscaleAccessibilityService.enableGrayscale(applicationContext)
-              result.success(null)
-          } else if (call.method == "disableGrayscale") {
-              GrayscaleAccessibilityService.disableGrayscale(applicationContext)
-              result.success(null)
-          } else {
-              result.notImplemented()
-          }
-      }
-  }
+        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "enableGrayscale" -> {
+                    GrayscaleAccessibilityService.enableGrayscale(applicationContext)
+                    result.success(null)
+                }
+                "disableGrayscale" -> {
+                    GrayscaleAccessibilityService.disableGrayscale(applicationContext)
+                    result.success(null)
+                }
+                else -> result.notImplemented()
+            }
+        }
+    }
 }
