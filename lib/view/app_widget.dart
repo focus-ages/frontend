d32aaa944035032e5ad91719653bbf78018a2c24
Pedro Iglesias <pedro.iglesias@edu.pucrs.br>
2023-05-18 15:09:49 -0300

    first commit

--- lib/view/app_widget.dart
+++ lib/view/app_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:front/view/cadastro/form_view.dart';
 import 'package:front/view/home/home_view.dart';
 import 'package:front/view/home/splash_screen_view.dart';
+import 'package:front/view/home/teste.dart';
 import 'package:front/view/mensagens/mensagens_view.dart';
 
 class AppWidget extends StatelessWidget {
@@ -10,10 +11,11 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/',
+      initialRoute: '/teste',
       routes: {
         '/': (context) => const Splash(),
         '/home': (context) => const HomePage(),
+        '/teste': (context) => const BancoPage(),
         '/cadastro': (context) => const Cadastro(),
         '/mensagens': (context) => const Mensagens(),
       },
---
+++ lib/view/home/teste.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import '../../components/home_componenets/custom_button.dart';
+import '../../resources/color_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BancoPage extends StatefulWidget {
+  const BancoPage({Key? key}) : super(key: key);
+
+  @override
+  State<BancoPage> createState() => _BancoPage();
+}
+
+void testeCinza() {
+  const platform = MethodChannel(
+      'channel_name'); // Substitua 'channel_name' pelo nome do canal que você definir no lado nativo (Android).
+
+  try {
+    platform.invokeMethod(
+        'setAccessibilitySetting'); // Substitua 'setAccessibilitySetting' pelo nome do método que você definir no lado nativo.
+  } on PlatformException catch (e) {
+    print(
+        'Ocorreu um erro ao alterar a configuração de acessibilidade: ${e.message}');
+  }
+}
+
+class _BancoPage extends State<BancoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exemplo de Tela'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            testeCinza();
+          },
+          child: Text('Teste cinza'),
+        ),
+      ),
+    );
+  }
+}
