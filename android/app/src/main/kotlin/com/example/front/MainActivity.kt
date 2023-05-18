80048978e69c56bc6dcb8b36a0d16e7918871f0e
Pedro Iglesias <pedrocb.iglesias@gmail.com>
2023-05-18 20:36:21 -0300

    testando acessibilidade android

--- android/app/src/main/kotlin/com/example/front/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/front/MainActivity.kt
@@ -1,29 +1,36 @@
 package com.example.front
 
 import android.os.Bundle
-import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : FlutterActivity() {
     private val CHANNEL = "channel_name"
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
 
-        MethodChannel(flutterEngine!!.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
-            when (call.method) {
-                "enableGrayscale" -> {
-                    GrayscaleAccessibilityService.enableGrayscale(applicationContext)
-                    result.success(null)
-                }
-                "disableGrayscale" -> {
-                    GrayscaleAccessibilityService.disableGrayscale(applicationContext)
-                    result.success(null)
-                }
-                else -> result.notImplemented()
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "enableGrayscale") {
+                enableGrayscale()
+                result.success(null)
+            } else {
+                result.notImplemented()
             }
+        }
+    }
+
+    private fun enableGrayscale() {
+        GlobalScope.launch(Dispatchers.Main) {
+            // Verifique se você tem as permissões necessárias aqui
+            // ...
+
+            Util.toggleGrayscale(applicationContext, !Util.isGrayscaleEnabled(applicationContext))
+            finish()
         }
     }
 }
--- lib/view/home/teste.dart
+++ lib/view/home/teste.dart
@@ -6,87 +6,83 @@ import '../../resources/color_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-
 class BancoPage extends StatefulWidget {
   const BancoPage({Key? key}) : super(key: key);
-
 
   @override
   State<BancoPage> createState() => _BancoPage();
 }
 
-
-void testeCinza() {
-  const platform = MethodChannel('channel_name'); // Substitua 'channel_name' pelo nome do canal que você definir no lado nativo (Android).
+void testeCinza() async {
+  const platform = MethodChannel(
+      'channel_name'); // Substitua 'channel_name' pelo nome do canal que você definir no lado nativo (Android).
 
   try {
-    platform.invokeMethod('enableGrayscale'); // Substitua 'setAccessibilitySetting' pelo nome do método que você definir no lado nativo.
+    await platform.invokeMethod('enableGrayscale');
   } on PlatformException catch (e) {
-    print('Ocorreu um erro ao alterar a configuração de acessibilidade: ${e.message}');
+    print('Error: $e');
   }
 }
-
 
 class _BancoPage extends State<BancoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorPattern.darkMode,
-      body: Center (child:
-        Column(
-          mainAxisSize: MainAxisSize.min,
-          children:[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: Center(
+          child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: const [
-            CustomButton(
-              onPressed: testeCinza,
-              title: Text(
-                "testeCinza",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color:Colors.white,
+              CustomButton(
+                onPressed: testeCinza,
+                title: Text(
+                  "testeCinza",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],  
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-            CustomButton(
-              onPressed: testeCinza,
-              title: Text(
-                "testeCinza",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color:Colors.white,
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CustomButton(
+                onPressed: testeCinza,
+                title: Text(
+                  "testeCinza",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],  
-            ),
-            SizedBox(height: 10),     
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-            CustomButton(
-              onPressed: testeCinza,
-              title: Text(
-                "testeCinza",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color:Colors.white,
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CustomButton(
+                onPressed: testeCinza,
+                title: Text(
+                  "testeCinza",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-            ),
-          ],  
-            ),
-            SizedBox(height: 10)
-          ],
-        )
-      ),
+            ],
+          ),
+          SizedBox(height: 10)
+        ],
+      )),
     );
   }
 }
