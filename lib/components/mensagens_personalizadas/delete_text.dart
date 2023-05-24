5c6cd8172ed1da9c4c76bd222595614c60e94dd1
Rafael Cardoso <Rafaelcardoso2604@gmail.com>
2023-05-23 23:57:32 -0300

    fix delete text

--- lib/components/mensagens_personalizadas/delete_text.dart
+++ lib/components/mensagens_personalizadas/delete_text.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:front/resources/text_styles.dart';
-import 'package:flutter/rendering.dart';
-import '../../model/user_model.dart';
-import '../../resources/color_pattern.dart';
+  import 'package:flutter/material.dart';
+  import 'package:flutter/rendering.dart';
+  import '../../model/user_model.dart';
+  import '../../resources/color_pattern.dart';
 
 class DeleteText extends StatefulWidget {
   final String message;
@@ -19,7 +18,6 @@ class DeleteText extends StatefulWidget {
   @override
   _DeleteTextState createState() => _DeleteTextState();
 }
-
 class _DeleteTextState extends State<DeleteText> {
   final User_model userModel = User_model();
   final TextEditingController _messageController = TextEditingController();
@@ -28,26 +26,19 @@ class _DeleteTextState extends State<DeleteText> {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: ColorPattern.darkCard,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      content: Text("${widget.placeholder}",
-          style:
-              CustomTextStylesBuilder().withColor(ColorPattern.white).title1()),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () => {
-            widget.onDelete(this.widget.message),
-            Navigator.pop(context),
-          },
-          child:
-              Text("Sim", style: CustomTextStylesBuilder().whitePopUpButton()),
-        ),
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: Text("Não", style: CustomTextStylesBuilder().greenPopUpButton()),
+Size size = MediaQuery.of(context).size;
+
+    return LayoutBuilder(builder: (context, constraints) {
+      final double fontSize = constraints.maxWidth * 0.05;
+      final TextStyle textStyle = TextStyle(
+        color: ColorPattern.white,
+        fontSize: fontSize,
+      );
+
+      return AlertDialog(
+        backgroundColor: ColorPattern.darkCard,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
         ),
         insetPadding: const EdgeInsets.symmetric(horizontal: 32.0),
           content: Text(
